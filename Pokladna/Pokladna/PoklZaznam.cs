9eea8a1a3c2295a26fa2316bff2c7034cf3e9bfd
Lukáš Bukaj <Bukaj.Lukas@stud.vassboskovice.cz>
2020-10-13 09:11:19 +0200

    PoklZaznam

---
+++ Pokladna/Pokladna/PoklZaznam.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokladna
+{
+    public class PoklZaznam
+    {
+        
+
+        public int IdPoklZaz { get; set; }
+        public int Cislo { get; set; }
+        public DateTime Datum { get; set; }
+        public string Popis { get; set; }
+        public double Castka { get; set; }
+        public double Zustatek { get; set; }
+        public string Poznamka { get; set; }
+
+        public PoklZaznam(int idPoklZaz, int cislo, DateTime datum, string popis, double castka, double zustatek, string poznamka)
+        {
+            IdPoklZaz = idPoklZaz;
+            Cislo = cislo;
+            Datum = datum;
+            Popis = popis;
+            Castka = castka;
+            Zustatek = zustatek;
+            Poznamka = poznamka;
+        }
+
+    }
+}
