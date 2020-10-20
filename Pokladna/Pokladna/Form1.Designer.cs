2d2c993c526b3ec80785a132ba1b18add4970ffd
Lukáš Bukaj <Bukaj.Lukas@stud.vassboskovice.cz>
2020-10-20 09:39:25 +0200

    opravaChyb

--- Pokladna/Pokladna/Form1.Designer.cs
+++ Pokladna/Pokladna/Form1.Designer.cs
@@ -28,132 +28,52 @@
   /// </summary>
   private void InitializeComponent()
   {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.lvData = new System.Windows.Forms.ListView();
-            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.cbRok = new System.Windows.Forms.ComboBox();
-            this.cbMes = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.panel1.SuspendLayout();
-            this.groupBox1.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Controls.Add(this.groupBox1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel1.Location = new System.Drawing.Point(748, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(274, 698);
-            this.panel1.TabIndex = 0;
-            // 
-            // lvData
-            // 
-            this.lvData.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnHeader1,
-            this.columnHeader2,
-            this.columnHeader3,
-            this.columnHeader4,
-            this.columnHeader5,
-            this.columnHeader6,
-            this.columnHeader7});
-            this.lvData.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lvData.GridLines = true;
-            this.lvData.HideSelection = false;
-            this.lvData.Location = new System.Drawing.Point(0, 0);
-            this.lvData.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.lvData.Name = "lvData";
-            this.lvData.Size = new System.Drawing.Size(748, 698);
-            this.lvData.TabIndex = 1;
-            this.lvData.UseCompatibleStateImageBehavior = false;
-            this.lvData.View = System.Windows.Forms.View.Details;
-            // 
-            // columnHeader1
-            // 
-            this.columnHeader1.Text = "Datum";
-            // 
-            // columnHeader2
-            // 
-            this.columnHeader2.Text = "Č. dokladu";
-            this.columnHeader2.Width = 97;
-            // 
-            // columnHeader3
-            // 
-            this.columnHeader3.Text = "Popis";
-            this.columnHeader3.Width = 290;
-            // 
-            // columnHeader4
-            // 
-            this.columnHeader4.Text = "Příjmy";
-            this.columnHeader4.Width = 95;
-            // 
-            // columnHeader5
-            // 
-            this.columnHeader5.Text = "Výdaje";
-            this.columnHeader5.Width = 114;
-            // 
-            // columnHeader6
-            // 
-            this.columnHeader6.Text = "Zůstatek";
-            this.columnHeader6.Width = 146;
-            // 
-            // columnHeader7
-            // 
-            this.columnHeader7.Text = "Poznámka";
-            this.columnHeader7.Width = 192;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.cbMes);
-            this.groupBox1.Controls.Add(this.cbRok);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(4, 3);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(247, 143);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Účetní období";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 34);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(27, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Rok";
-            // 
-            // cbRok
-            // 
-            this.cbRok.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbRok.FormattingEnabled = true;
-            this.cbRok.Items.AddRange(new object[] {
-            "2018",
-            "2019",
-            "2020",
-            "2021"});
-            this.cbRok.Location = new System.Drawing.Point(88, 31);
-            this.cbRok.Name = "cbRok";
-            this.cbRok.Size = new System.Drawing.Size(129, 21);
-            this.cbRok.TabIndex = 1;
-            this.cbRok.SelectedIndexChanged += new System.EventHandler(this.cbRok_SelectedIndexChanged);
-            // 
-            // cbMes
-            // 
-            this.cbMes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbMes.FormattingEnabled = true;
-            this.cbMes.Items.AddRange(new object[] {
+   this.panel1 = new System.Windows.Forms.Panel();
+   this.gBoxObdobi = new System.Windows.Forms.GroupBox();
+   this.cBoxMesic = new System.Windows.Forms.ComboBox();
+   this.cBoxRok = new System.Windows.Forms.ComboBox();
+   this.label2 = new System.Windows.Forms.Label();
+   this.label1 = new System.Windows.Forms.Label();
+   this.lvData = new System.Windows.Forms.ListView();
+   this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+   this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+   this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+   this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+   this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+   this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+   this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+   this.panel1.SuspendLayout();
+   this.gBoxObdobi.SuspendLayout();
+   this.SuspendLayout();
+   // 
+   // panel1
+   // 
+   this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+   this.panel1.Controls.Add(this.gBoxObdobi);
+   this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+   this.panel1.Location = new System.Drawing.Point(998, 0);
+   this.panel1.Name = "panel1";
+   this.panel1.Size = new System.Drawing.Size(365, 859);
+   this.panel1.TabIndex = 0;
+   // 
+   // gBoxObdobi
+   // 
+   this.gBoxObdobi.Controls.Add(this.cBoxMesic);
+   this.gBoxObdobi.Controls.Add(this.cBoxRok);
+   this.gBoxObdobi.Controls.Add(this.label2);
+   this.gBoxObdobi.Controls.Add(this.label1);
+   this.gBoxObdobi.Location = new System.Drawing.Point(5, 0);
+   this.gBoxObdobi.Name = "gBoxObdobi";
+   this.gBoxObdobi.Size = new System.Drawing.Size(355, 97);
+   this.gBoxObdobi.TabIndex = 0;
+   this.gBoxObdobi.TabStop = false;
+   this.gBoxObdobi.Text = "Účetní období";
+   // 
+   // cBoxMesic
+   // 
+   this.cBoxMesic.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+   this.cBoxMesic.FormattingEnabled = true;
+   this.cBoxMesic.Items.AddRange(new object[] {
             "Leden",
             "Únor",
             "Březen",
@@ -166,36 +86,113 @@
             "Říjen",
             "Listopad",
             "Prosinec"});
-            this.cbMes.Location = new System.Drawing.Point(88, 58);
-            this.cbMes.Name = "cbMes";
-            this.cbMes.Size = new System.Drawing.Size(129, 21);
-            this.cbMes.TabIndex = 2;
-            this.cbMes.SelectedIndexChanged += new System.EventHandler(this.cbMes_SelectedIndexChanged);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 61);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(37, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Měsíc";
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1022, 698);
-            this.Controls.Add(this.lvData);
-            this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.panel1.ResumeLayout(false);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
-            this.ResumeLayout(false);
+   this.cBoxMesic.Location = new System.Drawing.Point(81, 59);
+   this.cBoxMesic.Name = "cBoxMesic";
+   this.cBoxMesic.Size = new System.Drawing.Size(148, 24);
+   this.cBoxMesic.TabIndex = 3;
+   this.cBoxMesic.SelectedIndexChanged += new System.EventHandler(this.cBoxRok_SelectedIndexChanged);
+   // 
+   // cBoxRok
+   // 
+   this.cBoxRok.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+   this.cBoxRok.FormattingEnabled = true;
+   this.cBoxRok.Items.AddRange(new object[] {
+            "2019",
+            "2020",
+            "2021"});
+   this.cBoxRok.Location = new System.Drawing.Point(81, 26);
+   this.cBoxRok.Name = "cBoxRok";
+   this.cBoxRok.Size = new System.Drawing.Size(148, 24);
+   this.cBoxRok.TabIndex = 2;
+   this.cBoxRok.SelectedIndexChanged += new System.EventHandler(this.cBoxRok_SelectedIndexChanged);
+   // 
+   // label2
+   // 
+   this.label2.AutoSize = true;
+   this.label2.Location = new System.Drawing.Point(22, 59);
+   this.label2.Name = "label2";
+   this.label2.Size = new System.Drawing.Size(44, 17);
+   this.label2.TabIndex = 1;
+   this.label2.Text = "Měsíc";
+   // 
+   // label1
+   // 
+   this.label1.AutoSize = true;
+   this.label1.Location = new System.Drawing.Point(22, 29);
+   this.label1.Name = "label1";
+   this.label1.Size = new System.Drawing.Size(33, 17);
+   this.label1.TabIndex = 0;
+   this.label1.Text = "Rok";
+   // 
+   // lvData
+   // 
+   this.lvData.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7});
+   this.lvData.Dock = System.Windows.Forms.DockStyle.Fill;
+   this.lvData.FullRowSelect = true;
+   this.lvData.GridLines = true;
+   this.lvData.HideSelection = false;
+   this.lvData.Location = new System.Drawing.Point(0, 0);
+   this.lvData.Name = "lvData";
+   this.lvData.Size = new System.Drawing.Size(998, 859);
+   this.lvData.TabIndex = 1;
+   this.lvData.UseCompatibleStateImageBehavior = false;
+   this.lvData.View = System.Windows.Forms.View.Details;
+   // 
+   // columnHeader1
+   // 
+   this.columnHeader1.Text = "Datum";
+   // 
+   // columnHeader2
+   // 
+   this.columnHeader2.Text = "Č. dokladu";
+   this.columnHeader2.Width = 97;
+   // 
+   // columnHeader3
+   // 
+   this.columnHeader3.Text = "Popis";
+   this.columnHeader3.Width = 290;
+   // 
+   // columnHeader4
+   // 
+   this.columnHeader4.Text = "Příjmy";
+   this.columnHeader4.Width = 95;
+   // 
+   // columnHeader5
+   // 
+   this.columnHeader5.Text = "Výdaje";
+   this.columnHeader5.Width = 114;
+   // 
+   // columnHeader6
+   // 
+   this.columnHeader6.Text = "Zůstatek";
+   this.columnHeader6.Width = 146;
+   // 
+   // columnHeader7
+   // 
+   this.columnHeader7.Text = "Poznámka";
+   this.columnHeader7.Width = 192;
+   // 
+   // Form1
+   // 
+   this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+   this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+   this.ClientSize = new System.Drawing.Size(1363, 859);
+   this.Controls.Add(this.lvData);
+   this.Controls.Add(this.panel1);
+   this.Name = "Form1";
+   this.Text = "Form1";
+   this.Load += new System.EventHandler(this.Form1_Load);
+   this.panel1.ResumeLayout(false);
+   this.gBoxObdobi.ResumeLayout(false);
+   this.gBoxObdobi.PerformLayout();
+   this.ResumeLayout(false);
 
   }
 
@@ -210,11 +207,11 @@
   private System.Windows.Forms.ColumnHeader columnHeader5;
   private System.Windows.Forms.ColumnHeader columnHeader6;
   private System.Windows.Forms.ColumnHeader columnHeader7;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox cbMes;
-        private System.Windows.Forms.ComboBox cbRok;
-        private System.Windows.Forms.Label label1;
-    }
+  private System.Windows.Forms.GroupBox gBoxObdobi;
+  private System.Windows.Forms.ComboBox cBoxMesic;
+  private System.Windows.Forms.ComboBox cBoxRok;
+  private System.Windows.Forms.Label label2;
+  private System.Windows.Forms.Label label1;
+ }
 }
 
--- Pokladna/Pokladna/Form1.cs
+++ Pokladna/Pokladna/Form1.cs
@@ -10,56 +10,48 @@ using System.Windows.Forms;
 
 namespace Pokladna
 {
-    public partial class Form1 : Form
+ public partial class Form1 : Form
+ {
+  List<PokladniZaznam> pokladna;
+  IRepos repositar;
+
+  public Form1()
+  {
+   InitializeComponent();
+  }
+
+  private void Form1_Load(object sender, EventArgs e)
+  {
+   JsonRepos jsonRepos = new JsonRepos("data.json");
+   jsonRepos.VytvorTestData();
+   repositar = jsonRepos;
+
+   cBoxRok.SelectedIndex = cBoxRok.Items.IndexOf(DateTime.Now.Year.ToString());
+   cBoxMesic.SelectedIndex = DateTime.Now.Month-1;
+
+
+
+
+   //repositar = new SqlRepos();
+   //repositar = new XmlRepos();
+   //pokladna = repositar.NactiVse();
+   //foreach (var p in pokladna)
+   //{
+   // lvData.Items.Add(p.DoLvItem());
+   //}
+  }
+
+  private void cBoxRok_SelectedIndexChanged(object sender, EventArgs e)
+  {
+   if (cBoxRok.SelectedIndex >= 0 && cBoxMesic.SelectedIndex >= 0)
+   {
+    pokladna = repositar.NactiMesic(int.Parse(cBoxRok.SelectedItem.ToString()), cBoxMesic.SelectedIndex + 1);
+    lvData.Items.Clear();
+    foreach (var p in pokladna)
     {
-        List<PokladniZaznam> pokladna;
-        IRepos repositar;
-
-        public Form1()
-        {
-            InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            cbRok.SelectedIndex = cbRok.Items.IndexOf(DateTime.Now.Year.ToString());
-            cbMes.SelectedIndex = DateTime.Now.Month - 1;
-            JsonRepos jsonRepos = new JsonRepos("data.json");
-            jsonRepos.VytvorTestData();
-            repositar = jsonRepos;
-
-            
-            //repositar = new SqlRepos();
-            //repositar = new XmlRepos();
-            //pokladna = repositar.NactiVse();
-            //foreach (var p in pokladna)
-            //{
-            //    lvData.Items.Add(p.DoLvItem());
-            //}
-        }
-
-        private void cbRok_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-            if (cbRok.SelectedIndex >= 0)
-            {
-                pokladna = repositar.NactiMesic(int.Parse(cbRok.SelectedItem.ToString()), cbMes.SelectedIndex + 1);
-                lvData.Items.Clear();
-                foreach (var p in pokladna)
-                {
-                    lvData.Items.Add(p.DoLvItem());
-                }
-            }
-        }
-
-        private void cbMes_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            pokladna = repositar.NactiMesic(int.Parse(cbRok.SelectedText), cbMes.SelectedIndex + 1);
-            if (cbRok.SelectedIndex >= 0)
-                foreach (var p in pokladna)
-                {
-                    lvData.Items.Add(p.DoLvItem());
-                }
-        }
+     lvData.Items.Add(p.DoLvItem());
     }
+   }
+  }
+ }
 }
